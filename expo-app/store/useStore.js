9cb298b89527ccbe3a7c079f3897f0bb00bf590a
okaseobari <135550689+okaseobari@users.noreply.github.com>
2024-10-27 16:57:46 -0500

    more configs

---
+++ expo-app/store/useStore.js
@@ -0,0 +1,71 @@
+import { create } from "zustand";
+import axiosInstance from "../services/axiosInstance";
+
+const useStore = create((set) => ({
+  contacts: [],
+  schedule: [],
+  loading: false,
+
+  fetchContacts: async () => {
+    set({ loading: true });
+    try {
+      const response = await axiosInstance.get("/contact");
+      set({ contacts: response.data });
+    } catch (error) {
+      console.error("Failed to fetch contacts:", error);
+    } finally {
+      set({ loading: false });
+    }
+  },
+
+  addContact: async (contactData) => {
+    try {
+      const response = await axiosInstance.post("/contact", contactData);
+      set((state) => ({ contacts: [...state.contacts, response.data] }));
+    } catch (error) {
+      console.error("Failed to add contact:", error);
+    }
+  },
+
+  updateContact: async (contactId, updatedData) => {
+    try {
+      const response = await axiosInstance.put(
+        `/contact/${contactId}`,
+        updatedData
+      );
+      set((state) => ({
+        contacts: state.contacts.map((contact) =>
+          contact._id === contactId ? { ...contact, ...updatedData } : contact
+        ),
+      }));
+    } catch (error) {
+      console.error("Failed to update contact:", error);
+    }
+  },
+
+  deleteContact: async (contactId) => {
+    try {
+      await axiosInstance.delete(`/contact/${contactId}`);
+      set((state) => ({
+        contacts: state.contacts.filter((contact) => contact._id !== contactId),
+      }));
+    } catch (error) {
+      console.error("Failed to delete contact:", error);
+    }
+  },
+
+  fetchUserSchedule: async () => {
+    set({ loading: true });
+    try {
+      const response = await axiosInstance.get("/schedule");
+      set({ schedule: response.data, loading: false });
+    } catch (error) {
+      console.error("Error fetching schedule:", error);
+      set({ loading: false });
+    }
+  },
+
+  setSchedule: (newSchedule) => set({ schedule: newSchedule }),
+}));
+
+export default useStore;
